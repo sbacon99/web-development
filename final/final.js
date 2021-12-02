8c3cf00e629ef8fd5f65ac7e69306591c1072f28
Sam Bacon <sbacon3@elon.edu>
2021-12-01 22:32:34 -0500

    final

---
+++ final/final.js
@@ -0,0 +1,8 @@
+
+
+// call function repeatedly every t milliseconds
+let timerId2 = setTimeout(myFunc2, 2000);
+
+function myFunc2() {
+    document.getElementById("cookiesPopup").style.display = "inline";
+}
